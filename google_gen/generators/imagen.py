6567f487a5db35d3141bf38e9e06501c77076ea5
Miłosz Gaczkowski <milosz+github@omgomg.eu>
2026-03-16 17:12:47 +0000

    chore: change default imagen model to imagen-4.0-ultra-generate-001

--- google_gen/generators/imagen.py
+++ google_gen/generators/imagen.py
@@ -10,8 +10,8 @@ class Imagen(BaseGenerator):
         parser.add_argument(
             "-m",
             "--model",
-            default="imagen-4.0-generate-001",
-            help="The Imagen model to use (e.g., imagen-3.0-generate-001, imagen-3.0-fast-generate-001, imagen-4.0-generate-001) (default: %(default)s)",
+            default="imagen-4.0-ultra-generate-001",
+            help="The Imagen model to use (e.g., imagen-4.0-generate-001, imagen-4.0-fast-generate-001, imagen-4.0-ultra-generate-001) (default: %(default)s)",
         )
         parser.add_argument(
             "-b",
